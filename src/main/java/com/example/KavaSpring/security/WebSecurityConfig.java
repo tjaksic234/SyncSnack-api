82f804c8cf9103d66e9d068f64efa241947f35ac
tjaksic234 <jaksa.teo@gmail.com>
2024-07-11 08:41:44 +0200

    undo the auth route for users

--- src/main/java/com/example/KavaSpring/security/WebSecurityConfig.java
+++ src/main/java/com/example/KavaSpring/security/WebSecurityConfig.java
@@ -53,7 +53,6 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/users/**").permitAll()
                         .anyRequest().authenticated());
 
         http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
