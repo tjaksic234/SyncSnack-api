b2cd438ae3d138150da721a8cee18bedc597c955
tjaksic234 <jaksa.teo@gmail.com>
2024-10-08 13:38:16 +0200

    added a security req for calling swagger endpoints

--- src/main/java/com/example/KavaSpring/config/openapi/OpenAPIConfig.java
+++ src/main/java/com/example/KavaSpring/config/openapi/OpenAPIConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,8 +25,7 @@ public class OpenAPIConfig {
                 .info(new Info()
                         .title("SyncSnack")
                         .description("OpenAPI doc")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"));
     }
-
-
 }
