d1ab0dd3f8572453eb3beab5a21f8cdf3d561bc6
tjaksic234 <jaksa.teo@gmail.com>
2024-10-08 14:02:35 +0200

    modified the Order DAO

--- src/main/java/com/example/KavaSpring/models/dao/Order.java
+++ src/main/java/com/example/KavaSpring/models/dao/Order.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -36,6 +37,7 @@ public class Order {
     @NotBlank
     private String groupId;
 
+    @TextIndexed
     private HashMap<String, Object> additionalOptions = new HashMap<>() {{
         put("description", "");
     }};
