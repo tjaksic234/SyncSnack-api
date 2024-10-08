83327bdc617f7e2da430647fdc59f6a27feb7da8
tjaksic234 <jaksa.teo@gmail.com>
2024-10-08 14:34:01 +0200

    opened the swagger docs endpoint

--- src/main/java/com/example/KavaSpring/security/WebSecurityConfig.java
+++ src/main/java/com/example/KavaSpring/security/WebSecurityConfig.java
@@ -49,6 +49,8 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/.well-known/**").permitAll() //? for mobile functionality
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("v3/**").permitAll()
                         .requestMatchers("api/profiles/create").permitAll()
                         .requestMatchers("api/orders/create").permitAll()
                         .requestMatchers("api/events/create").permitAll()
@@ -59,7 +61,6 @@ public class WebSecurityConfig {
                         .requestMatchers("api/groups/joinViaInvitation/{code}").permitAll()
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/ws").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").authenticated()
                         .anyRequest().authenticated());
         http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
